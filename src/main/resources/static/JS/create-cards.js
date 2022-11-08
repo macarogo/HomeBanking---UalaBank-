2d59a374bfda112118a5ddacafd3217aa14eccf0
macarogo <mariacarolinagomez88@gmail.com>
2022-11-08 11:22:45 -0300

    modify Js cards

--- src/main/resources/static/JS/create-cards.js
+++ src/main/resources/static/JS/create-cards.js
@@ -8,45 +8,37 @@ Vue.createApp({
        cardCredit:"",
        cardDebit:"",
        limiteCardDebit:"",
-     }
+      }
     },
-  
       created(){
           axios.get("/api/clients/current")
           .then(datos =>{
-              this.client = datos.data
-              this.cards = datos.data.cards
-              this.cardDebit = this.cards.filter(card => card.type == "DEBITO")
-              this.cardCredit = this.cards.filter(card => card.type == "CREDITO")
-
+            this.client = datos.data
+            this.cards = datos.data.cards
+            this.cardDebit = this.cards.filter(card => card.type == "DEBITO")
+            this.cardCredit = this.cards.filter(card => card.type == "CREDITO")
           })
       },
-  
     methods : {
-
       createCards(){
         console.log(this.cardType)
         if(this.cardType == false){
-          axios.post('/api/clients/current/cards', `cardType=DEBITO&cardColor=${this.cardColor}`,
-          {headers:{'content-type': 'application/x-www-form-urlencoded'}})
+          axios.post('/api/clients/current/cards', `cardType=DEBITO&cardColor=${this.cardColor}`)
           .then(response => window.location.href="/web/cards.html")
           .catch(error=> {
             this.limiteCardDebit="I exceed the number of Debit card limits"
           })
         }
         else {
-          axios.post('/api/clients/current/cards', `cardType=CREDITO&cardColor=${this.cardColor}`,
-          {headers:{'content-type': 'application/x-www-form-urlencoded'}})
+          axios.post('/api/clients/current/cards', `cardType=CREDITO&cardColor=${this.cardColor}`)
           .then(response => window.location.href="/web/cards.html")
           .catch(error=>{
             this.limiteCardCredit="I exceed the number of Credit card limits"
           })
         }
-
       },
+    },
 
-  },
-  
     computed:{},
   
   }).mount('#app')
