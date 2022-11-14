d82cfbb61d057b46b6d96af63d875e017b09dc4f
macarogo <mariacarolinagomez88@gmail.com>
2022-11-14 10:34:40 -0300

    cleaning code tests

--- src/test/java/com/mindhub/homebanking/HomebankingApplicationTests.java
+++ src/test/java/com/mindhub/homebanking/HomebankingApplicationTests.java
@@ -9,5 +9,4 @@ class HomebankingApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
 }
--- src/test/java/com/mindhub/homebanking/RepositoriesTest.java
+++ src/test/java/com/mindhub/homebanking/RepositoriesTest.java
@@ -23,122 +23,74 @@ import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTest
 @AutoConfigureTestDatabase(replace = NONE)
 
 public class RepositoriesTest {
-
     @Autowired
     private ClientRepository clientRepository;
-
     @Autowired
     private AccountRepository accountRepository;
-
     @Autowired
     private CardRepository cardRepository;
-
     @Autowired
     private TransactionRepository transactionRepository;
-
     @Autowired
     private LoanRepository loanRepository;
 
-
     @Test
-
     public void existClient(){
-
         List<Client> clients = clientRepository.findAll();
-
         assertThat(clients,is(not(empty())));
-
     }
 
     @Test
-
     public void existClientLorenzo(){
-
         List<Client> clients = clientRepository.findAll();
         assertThat(clients, hasItem(hasProperty("firstName", is("Lorenzo"))));
-
     }
 
     @Test
-
     public void existAccount(){
-
         List<Account> accounts = accountRepository.findAll();
-
         assertThat(accounts,is(not(empty())));
-
     }
 
     @Test
-
     public void existAccountVIN001(){
-
         List<Account> accounts = accountRepository.findAll();
         assertThat(accounts,hasItem(hasProperty("number", is("VIN-001"))));
-
     }
 
     @Test
-
     public void existCard(){
-
         List<Card> cards = cardRepository.findAll();
-
         assertThat(cards,is(not(empty())));
-
     }
 
     @Test
-
     public void existCardLorenzoMalbel(){
-
         List<Card> cards = cardRepository.findAll();
         assertThat(cards, hasItem(hasProperty("cardHolder", is("Lorenzo Malbel"))));
-
     }
 
-
-
     @Test
-
     public void existTransaction(){
-
         List<Transaction> transactions = transactionRepository.findAll();
-
         assertThat(transactions,is(not(empty())));
-
     }
 
     @Test
-
     public void existPersonalTransaction(){
-
         List<Transaction> transactions = transactionRepository.findAll();
         assertThat(transactions, hasItem(hasProperty("description", is("Pago por Electricidad"))));
-
     }
 
-
     @Test
-
     public void existLoans(){
-
         List<Loan> loans = loanRepository.findAll();
-
         assertThat(loans,is(not(empty())));
-
     }
 
     @Test
-
     public void existPersonalLoan(){
-
         List<Loan> loans = loanRepository.findAll();
         assertThat(loans, hasItem(hasProperty("name", is("Personal"))));
-
     }
-
-
 }
-
-
